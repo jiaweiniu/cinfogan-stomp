ef4f13d2e4396a6f8d713d9ad9060c60bbed4cc8
thibault <thibault.barbie@free.fr>
2018-02-12 17:43:09 +0900

    Changed the plot.py file to adapt to any number of line

--- plot.py
+++ plot.py
@@ -11,31 +11,25 @@ ratio_lin=[]
 ratio_cgan=[]
 measure_lin=[]
 measure_cgan=[]
-t=np.linspace(2,200,199)
+n_lines=0
 
-
-f=open("results.dat",'r')
-for i in range(199):
-    str_tmp=str.split(f.readline().strip())
-    ratio_lin.append(float(str_tmp[0]))
-    measure_lin.append(float(str_tmp[1]))
-    ratio_cgan.append(float(str_tmp[2]))
-    measure_cgan.append(float(str_tmp[3]))
+with open("results.dat",'r') as f:
+    for i, l in enumerate(f):
+        str_tmp=str.split(l.strip())
+        ratio_lin.append(float(str_tmp[0]))
+        measure_lin.append(float(str_tmp[1]))
+        ratio_cgan.append(float(str_tmp[2]))
+        measure_cgan.append(float(str_tmp[3]))
+    n_lines=i+1
 
     
+t=np.linspace(2,n_lines+1,n_lines)
+
 plt.plot(t,ratio_lin,label='Linear')
 plt.plot(t,ratio_cgan,label='CGAN')
 
 plt.xlabel("Epoch")
 plt.ylabel("F1 measure")
-
-"""
-plt.plot(t,measure_lin,label='Linear')
-plt.plot(t,measure_cgan,label='CGAN')
-
-plt.xlabel("Epoch")
-plt.ylabel("F2 measure")
-"""
 
 plt.legend(loc='lower right')
 plt.show()
