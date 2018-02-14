Changed the f1 metric written in Python to use the Rust library rustmetric.so to increase the computation speed. Also fixed a small bug in extension where the denormalization was not done.

extensions.py
@@ -8,12 +8,14 @@ import import_dataset
 import matplotlib.patches as patches
 
 class GeneratorSample(extension.Extension):
-    def __init__(self, x_dim, xi_dim, noise_dim, dirname='samples', sample_format='png'):
+    def __init__(self, dataset_path, x_dim, xi_dim,
+                 noise_dim, dirname='samples', sample_format='png'):
         self._dirname = dirname
         self._sample_format = sample_format
         self.noise_dim=noise_dim
         self.x_dim=x_dim
         self.xi_dim=xi_dim
+        self.dataset_path=dataset_path
         
     def __call__(self, trainer):
         dirname = os.path.join(trainer.out, self._dirname)
@@ -21,7 +23,7 @@ class GeneratorSample(extension.Extension):
             n_gen=1
             z=Variable(np.random.uniform(-1,1,(n_gen,self.noise_dim)).astype(np.float32))
 
-            dataset=import_dataset.import_data(3,self.x_dim,self.xi_dim)
+            dataset=import_dataset.import_data(self.dataset_path,3,self.x_dim,self.xi_dim)
             x=Variable(dataset[1:2,:self.x_dim])
 
             xi_gen = trainer.updater.generator(z,x).data[0]
@@ -29,7 +31,7 @@ class GeneratorSample(extension.Extension):
             fig,ax = plt.subplots(1)
             
             x=x.data[0]
-            x=import_dataset.denormalize_data(x)
+            x=import_dataset.denormalize_data_random_left_right(x)
             
             start=[x[0],x[1]]
             goal=[x[2],x[3]]

testing_model.py
@@ -6,15 +6,18 @@ from scipy import interpolate
 from chainer import serializers, Variable
 
 import import_dataset
+import rustmetric
 
 # Measure the percentage of collision of the trajectory and compare it to the linear initialization
-def collisions_measure(x_list,xi_list):
+def collisions_measure(x_list,xi_pred_list):
     measure_lin_col=[]
     ratio_lin_col=[]
     measure_cgan_col=[]
     ratio_cgan_col=[]
-    
-    for x,xi in zip(x_list,xi_list):
+
+    list_xi_lin=[]
+    list_xi_cgan=[]
+    for x,xi_pred in zip(x_list,xi_pred_list):
         t=np.linspace(0,1,100)
         t=np.asarray([t])
 
@@ -27,52 +30,31 @@ def collisions_measure(x_list,xi_list):
 
         # cgan initialization
         t_xi=[0,0.333,0.5,0.644,1]
-        before_spline=np.array([[start[0][0],start[0][1]],[xi[0],xi[1]],[xi[2],xi[3]],[xi[4],xi[5]],[goal[0][0],goal[0][1]]])
+        before_spline=np.array([[start[0][0],start[0][1]],
+                                [xi_pred[0],xi_pred[1]],
+                                [xi_pred[2],xi_pred[3]],
+                                [xi_pred[4],xi_pred[5]],
+                                [goal[0][0],goal[0][1]]])
         
         spline,u=interpolate.splprep([before_spline[:,0],before_spline[:,1]],u=t_xi)
     
         cgan=interpolate.splev(t,spline)
         cgan=np.concatenate((np.transpose(cgan[0]),np.transpose(cgan[1])),axis=1)
 
-        obstacles=[ [x[4],x[5]], [x[6],x[7]], [x[8],x[9]], [x[10],x[11]] ]
+        list_xi_lin.append(lin.tolist())
+        list_xi_cgan.append(cgan.tolist())
+        
+    n_dim=2
+    n_obs=4
+    collision_limit=0.1
+
+    f1_lin  = rustmetric.f1(x_list.tolist(),list_xi_lin, n_dim,n_obs,collision_limit)
+    f1_cgan = rustmetric.f1(x_list.tolist(),list_xi_cgan,n_dim,n_obs,collision_limit)
+
+    f1_lin = 100.0*f1_lin/len(x_list)
+    f1_cgan = 100.0*f1_cgan/len(x_list)
     
-        measure_lin=0
-        lin_col_bool=False
-        for p in lin:
-            point_in_obs=False
-            for o in obstacles:
-                if np.linalg.norm(o-p)<0.1:
-                    point_in_obs=True
-            if(point_in_obs):
-                measure_lin+=1
-                lin_col_bool=True
-
-        if(lin_col_bool):
-            ratio_lin_col.append(1)
-        else:
-            ratio_lin_col.append(0)
-        measure_lin_col.append(measure_lin)
-
-        measure_cgan=0
-        cgan_col_bool=False
-        for p in cgan:
-            point_in_obs=False
-            for o in obstacles:
-                if np.linalg.norm(o-p)<0.1:
-                    point_in_obs=True
-            if(point_in_obs):
-                measure_cgan+=1
-                cgan_col_bool=True
-
-        if(cgan_col_bool):
-            ratio_cgan_col.append(1)
-        else:
-            ratio_cgan_col.append(0)
-        measure_cgan_col.append(measure_cgan)
-
-    return [100*np.mean(ratio_lin_col),np.mean(measure_lin_col),
-            100*np.mean(ratio_cgan_col),np.mean(measure_cgan_col),
-            100*(np.mean(ratio_lin_col)-np.mean(ratio_cgan_col))]
+    return [f1_lin, f1_cgan, f1_lin-f1_cgan]
             
 def test(gen,n_tests,noise_dim):
     z = np.random.uniform(0, 1, (n_tests, noise_dim))