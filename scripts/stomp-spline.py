0c8284364aeec82663d18329f0de60e2267f9a72
thibault <thibault.barbie@free.fr>
2018-11-07 14:30:27 +0900

    Added __main__ entry.

--- scripts/stomp-spline.py
+++ scripts/stomp-spline.py
@@ -112,77 +112,78 @@ def stomp(q_start,q_goal,n_timesteps,K,n_iter,obstacles,dt):
             
     return list_theta
 
-q_start=np.asarray([0.1,0.16])
-q_goal=np.asarray([0.76,0.88])
 
-n_timesteps=33
-n_timesteps1=2
-n_noisy=20
+if __name__ == '__main__':
+    q_start=np.asarray([0.1,0.16])
+    q_goal=np.asarray([0.76,0.88])
 
-dt=0.001/n_timesteps
+    n_timesteps=33
+    n_timesteps1=2
+    n_noisy=20
+
+    dt=0.001/n_timesteps
     
-A=np.zeros((n_timesteps+2,n_timesteps))
+    A=np.zeros((n_timesteps+2,n_timesteps))
 
-A[0][0]=1/dt
-A[1][0]=-2/dt
-A[1][1]=1/dt
+    A[0][0]=1/dt
+    A[1][0]=-2/dt
+    A[1][1]=1/dt
 
-A[n_timesteps+1][n_timesteps-1]=1/dt
-A[n_timesteps][n_timesteps-1]=-2/dt
-A[n_timesteps][n_timesteps-2]=1/dt
+    A[n_timesteps+1][n_timesteps-1]=1/dt
+    A[n_timesteps][n_timesteps-1]=-2/dt
+    A[n_timesteps][n_timesteps-2]=1/dt
     
-for i in range(n_timesteps-2):
-    A[i+2][i]=1/dt
-    A[i+2][i+1]=-2/dt
-    A[i+2][i+2]=1/dt
+    for i in range(n_timesteps-2):
+        A[i+2][i]=1/dt
+        A[i+2][i+1]=-2/dt
+        A[i+2][i+2]=1/dt
     
-R_inv=np.linalg.inv(np.dot(A.T,A))
-M=np.zeros((n_timesteps,n_timesteps))
-for i in range(n_timesteps):
-    M[:,i]=R_inv[:,i]/(n_timesteps*max(R_inv[:,i]))
+    R_inv=np.linalg.inv(np.dot(A.T,A))
+    M=np.zeros((n_timesteps,n_timesteps))
+    for i in range(n_timesteps):
+        M[:,i]=R_inv[:,i]/(n_timesteps*max(R_inv[:,i]))
 
-n_iter=30
+    n_iter=30
+    
+    obstacles=[[0.5,0.78],[0.6,0.5],[0.3,0.3]]
 
+    start_time=time.time()
+    traj_list=stomp(q_start,q_goal,n_timesteps,n_noisy,n_iter,obstacles,dt)
+    print()
+    print("--- %s seconds ---" %(time.time()-start_time))
 
-obstacles=[[0.5,0.78],[0.6,0.5],[0.3,0.3]]
+    fig=plt.figure()
+    ax = fig.add_subplot(111, aspect='equal')
 
-start_time=time.time()
-traj_list=stomp(q_start,q_goal,n_timesteps,n_noisy,n_iter,obstacles,dt)
-print()
-print("--- %s seconds ---" %(time.time()-start_time))
+    for obs in obstacles:
+        ax.add_patch(patches.Circle((obs[0], obs[1]),0.1,color='r',alpha=0.6))
 
-fig=plt.figure()
-ax = fig.add_subplot(111, aspect='equal')
-
-for obs in obstacles:
-    ax.add_patch(patches.Circle((obs[0], obs[1]),0.1,color='r',alpha=0.6))
-
-start=np.asarray([[q_start[0]],[q_start[1]]])
-goal=np.asarray([[q_goal[0]],[q_goal[1]]])
-for traj in traj_list:
-    final_traj=np.concatenate((start,traj),axis=1)
-    final_traj=np.concatenate((final_traj,goal),axis=1)
-
-
-line,=ax.plot([],[])
-points,=ax.plot([],[],'ro')
-
-plt.xlim(0.0,1.0)
-plt.ylim(0.0,1.0)
-
-def update(i,line):
-    concat_x=np.concatenate((np.concatenate((start[0],traj_list[i*2][0])),goal[0]))
-    concat_y=np.concatenate((np.concatenate((start[1],traj_list[i*2][1])),goal[1]))
-    line.set_data(concat_x,concat_y)
-    points.set_data(traj_list[i*2][0],traj_list[i*2][1])
-    return line,
-
-ani = animation.FuncAnimation(fig, update, frames=len(traj_list)//2,blit=False,fargs=[line])
-
-ax.get_xaxis().set_visible(False)
-ax.get_yaxis().set_visible(False)
+    start=np.asarray([[q_start[0]],[q_start[1]]])
+    goal=np.asarray([[q_goal[0]],[q_goal[1]]])
+    for traj in traj_list:
+        final_traj=np.concatenate((start,traj),axis=1)
+        final_traj=np.concatenate((final_traj,goal),axis=1)
 
 
-ani.save('cinfogan-stomp.gif', writer='imagemagick',fps=4)
+    line,=ax.plot([],[])
+    points,=ax.plot([],[],'ro')
 
-plt.show()
+    plt.xlim(0.0,1.0)
+    plt.ylim(0.0,1.0)
+
+    def update(i,line):
+        concat_x=np.concatenate((np.concatenate((start[0],traj_list[i*2][0])),goal[0]))
+        concat_y=np.concatenate((np.concatenate((start[1],traj_list[i*2][1])),goal[1]))
+        line.set_data(concat_x,concat_y)
+        points.set_data(traj_list[i*2][0],traj_list[i*2][1])
+        return line,
+
+    ani = animation.FuncAnimation(fig, update, frames=len(traj_list)//2,blit=False,fargs=[line])
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
+
+    ani.save('cinfogan-stomp.gif', writer='imagemagick',fps=4)
+
+    plt.show()
