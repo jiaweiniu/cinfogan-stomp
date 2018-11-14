21de9d7a4a758edbe1f5068e71b7554818651ab7
niujiawei <niujiawei1994@yahoo.co.jp>
2018-11-14 21:13:47 +0900

    generate points in random position.

--- scripts/cinfogan_initial_traj.py
+++ scripts/cinfogan_initial_traj.py
@@ -1,33 +1,74 @@
 # cinfogan initial trjaectory
+import os
 from scipy import interpolate
 from chainer import serializers
 import numpy as np
 from models import Generator
 from chainer import serializers, Variable
-
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
 
 
 def gen_points(gen,n_tests,n_z,obstacles,q_start,q_goal):
-    z = np.random.uniform(-2, 2, (n_tests, n_z+n_continuous))
+    z = np.random.uniform(-2, 2, (n_tests, n_z+n_continuous)) # noise and latent code
     z = Variable(np.array(z, dtype=np.float32))
-    x = np.random.uniform(0.0, 1.0, (n_tests,12));
+    x = np.random.uniform(0.0, 1.0, (n_tests,12));  # condition(problem) 
     x = Variable(x.astype(np.float32))
-    xi = gen(z,x)
-    return (xi)
+    xi = gen(z,x)  # generated points
+    return (xi.data[0])
 
-
+        
 
 if __name__ == '__main__':
-    gen = Generator(60,12,6,2,100)
-    obstacles = np.random.random((3,2))
-    q_start = np.random.random(2)
-    q_goal = np.random.random(2)
+    radius = 0.1
+    obstacles = np.random.random((3,2))    
+    wrong_setup=True
+    while wrong_setup:
+        q_start = np.random.random(2)
+        q_goal = np.random.random(2)
+        collide = False
+        for obs in obstacles:
+            if np.linalg.norm(q_start-obs)<radius or np.linalg.norm(q_goal-obs)<radius:
+                collide=True
+        if not(collide):
+            wrong_setup=False
+            '''
+or np.linalg.norm(obs[0]-obs[1])<radius or np.linalg.norm(obs[1]-obs[2])<radius or np.linalg.norm(obs[2]-obs[0])<radius:
+            '''
     n_tests = 500
     n_z = 60
     n_continuous = 2
-    serializers.load_npz("../results/models/tmp/21_gen.model",gen)
-    print (gen_points(gen,n_tests,n_z,obstacles,q_start,q_goal))
+    gen = Generator(60,12,6,2,100)
+    serializers.load_npz("../results/models/50.model",gen)
+# print obstacles, start, goal, generated points.
+    print("obstacles")
+    print(obstacles)
+    print("start")
+    print(q_start)
+    print("goal")
+    print(q_goal)
+    print("gen")
+    print(gen)
+    xi = gen_points(gen,n_tests,n_z,obstacles,q_start,q_goal)
+    print("generated points")
+    print (gen_points(gen,n_tests,n_z,obstacles,q_start,q_goal))   
+    
+# set obstacles
+    fig,ax = plt.subplots(1)  
+    circle =patches.Circle((obstacles[0]),radius=0.1,color='red',alpha=0.6)
+    ax.add_patch(circle)
+    circle = patches.Circle((obstacles[1]),radius=0.1,color='red',alpha=0.6)
+    ax.add_patch(circle)
+    circle = patches.Circle((obstacles[2]),radius=0.1,color='red',alpha=0.6)
+    ax.add_patch(circle)
 
+  
+    ax.plot([q_start[0],xi[0],xi[2],xi[4],q_goal[0]],[q_start[1],xi[1],xi[3],xi[5],q_goal[1]],marker='*')
+    ax.scatter(q_start[0],q_start[1],marker='s',s=130,color='black',zorder=20)
+    ax.scatter(q_goal[0],q_goal[1],marker='*',s=130,color='black',zorder=21)
+    ax.set_xlim((0,1))
+    ax.set_ylim((0,1))
+    plt.show()
 '''
 def cinfogan_initial_traj(q_start, q_goal, n_timesteps):
     t = np.linspace(0,1,n_timesteps)       # divided by timesteps times between 0 to 1    
