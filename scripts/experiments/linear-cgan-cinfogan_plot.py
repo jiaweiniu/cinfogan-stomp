fea8ce4b27ea467662e39342a06d6fbbf9f82040
thibault <thibault.barbie@free.fr>
2018-12-16 22:32:31 +0900

    Changed the file to make it easier to understand.

--- scripts/experiments/linear-cgan-cinfogan_plot.py
+++ scripts/experiments/linear-cgan-cinfogan_plot.py
@@ -6,50 +6,28 @@ from pandas.core.frame import DataFrame
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-f = open('../training/results/1.dat',"r")
-lists = []
+f = open('../training/results/f_metric.dat',"r")
+list_of_lines = []
 line = f.readline()
+n_epochs = 200
 
 while(line):
-    lists.append(line)
+    list_of_lines.append(line)
     line = f.readline()
 f.close
 
-lists = [i.split() for i in lists]
-v = ["epoch","method","F measure(%)"]
 
-for i in range (len(lists)):
-    v.append([i,'Linear',lists[i][0]])
-    v.append([i,'CGAN',lists[i][1]])
-    v.append([i,'CInfoGAN',lists[i][2]])
+list_of_lines = [i.split() for i in list_of_lines]
 
-newl = []
-for i in range (3,len(v)):
-    newl.append([int(v[i][0]),str(v[i][1]),float(v[i][2])])
+v=[]
+for i in range(len(list_of_lines)):
+    v.append([i-n_epochs*(i//n_epochs), 'Linear',   float(list_of_lines[i][0])])
+    v.append([i-n_epochs*(i//n_epochs), 'CGAN',     float(list_of_lines[i][1])])
+    v.append([i-n_epochs*(i//n_epochs), 'CInfoGAN', float(list_of_lines[i][2])])
 
-for i in range (600,1200):
-    newl[i][0] = newl[i][0] - 200
 
-for i in range (1200,1800):
-    newl[i][0] = newl[i][0] - 400
-
-for i in range (1800,2400):
-    newl[i][0] = newl[i][0] - 600
-
-for i in range (2400,3000):
-    newl[i][0] = newl[i][0] - 800
-
-for i in range (3000,3600):
-    newl[i][0] = newl[i][0] - 1000
-
-#name = ["epoch","method","acc"]
-name = ["epoch"," ","acc"]
-
-data = DataFrame(newl)
+data = DataFrame(v)
 data.columns = ['epoch','method','F measure(%)']
-
-
-print (data)
 
 sns.lineplot(x="epoch",y="F measure(%)",
              hue="method",data=data)
